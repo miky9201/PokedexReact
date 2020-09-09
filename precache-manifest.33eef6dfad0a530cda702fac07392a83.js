c5eecea3007a262ce7fbbe99575ee1c8d2eaaee8
Mike Baldacci <baldaccimike@gmail.com>
2020-09-09 17:06:20 +0200

    Updates

---
+++ precache-manifest.33eef6dfad0a530cda702fac07392a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41db7e0bc5835463727f96e169b4ffad",
+    "url": "/PokedexReact/index.html"
+  },
+  {
+    "revision": "3bb60f3542270710ccd8",
+    "url": "/PokedexReact/static/css/main.25d2303c.chunk.css"
+  },
+  {
+    "revision": "eede97e08383086e1c03",
+    "url": "/PokedexReact/static/js/2.07b9d80a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokedexReact/static/js/2.07b9d80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb60f3542270710ccd8",
+    "url": "/PokedexReact/static/js/main.eb37af45.chunk.js"
+  },
+  {
+    "revision": "e5ea79cf1271042f4896",
+    "url": "/PokedexReact/static/js/runtime-main.534a809a.js"
+  },
+  {
+    "revision": "a5b2d57acb70d3d021940e0828b92635",
+    "url": "/PokedexReact/static/media/pokemon.a5b2d57a.png"
+  }
+]);
